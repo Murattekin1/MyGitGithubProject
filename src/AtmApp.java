67b004f3d440ea583912b0eb0eb0a85b5b22c489
Murat Tekin <murattekin0216@gmail.com>
2023-02-24 00:13:07 +0300

    v4

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -5,5 +5,7 @@ public class AtmApp {
         System.out.println("java is easy");
         //verion 3
         System.out.println("Learn java earn money" );
+        //version 4
+        System.out.println("gitiogrenmek cok kolay");
     }
 }
